fb309362242d20f3972c5be4e929893fef390456
matthew123003 <matthew123003@gmail.com>
2024-07-21 15:25:56 -0400

    COntinued with React

--- FrontEndDevelopmentLibrariesCert/React-Projects/reactProject.js
+++ FrontEndDevelopmentLibrariesCert/React-Projects/reactProject.js
@@ -234,6 +234,31 @@ const JSX1 = (
     }
   };
 
+  const List = (props) => {
+    { /* Change code below this line */ }
+    return <p>{props.tasks.join(", ")}</p>
+    { /* Change code above this line */ }
+  };
+  
+  class ToDo extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>To Do Lists</h1>
+          <h2>Today</h2>
+          { /* Change code below this line */ }
+          <List tasks={["walk dog", "workout"]} />
+          <h2>Tomorrow</h2>
+          <List tasks={["buy groceries", "clean house", "study"]} />
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
+
   
 
 
