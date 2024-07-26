1fee1a5472f8f32b37807f7a41e89480d9dfb31d
matthew123003 <matthew123003@gmail.com>
2024-07-26 17:58:05 -0400

    Fetch and Promises

--- JS:CSS:HTML-Cert/fetchPromises/fetchPromises.js
+++ JS:CSS:HTML-Cert/fetchPromises/fetchPromises.js
@@ -1,2 +1,12 @@
 const authorContainer = document.getElementById('author-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
+
+/*The Fetch API is a built-in JavaScript interface to make network requests to a server. It has a fetch() method you can use to make GET, POST, PUT, or PATCH requests. In this project, you'll make a GET request to a URL for a JSON file with information about authors on freeCodeCamp News.
+
+Here is how you can make a GET request with the fetch() method:
+Example Code
+
+fetch("url-goes-here") */
+
+
+fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
