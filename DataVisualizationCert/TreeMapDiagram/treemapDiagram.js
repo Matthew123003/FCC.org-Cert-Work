db1e810d319a5103c7a6cff4ec2395709824ac21
matthew123003 <matthew123003@gmail.com>
2024-08-14 13:24:19 -0400

    Added rect elements using D3 SVG

---
+++ DataVisualizationCert/TreeMapDiagram/treemapDiagram.js
@@ -0,0 +1,11 @@
+const tiles = svg.selectAll(".tile")
+  .data(data)
+  .enter().append("rect") // Add rect elements
+  .attr("class", "tile") // Assign class for styling
+  .attr("x", d => xScale(d.x)) // Positioning based on data
+  .attr("y", d => yScale(d.y))
+  .attr("width", d => xScale(d.width)) // Size based on data
+  .attr("height", d => yScale(d.height))
+  .attr("data-name", d => d.name) // Add data attributes
+  .attr("data-category", d => d.category)
+  .attr("data-value", d => d.value);
