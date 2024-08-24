ed8b43af6a62788b2a9e5429cb99975b4d921968
matthew123003 <matthew123003@gmail.com>
2024-08-24 13:29:55 -0400

    Pass Callback as props

--- FrontEndDevelopmentLibrariesCert/React/reactProject.js
+++ FrontEndDevelopmentLibrariesCert/React/reactProject.js
@@ -726,6 +726,67 @@ class Navbar extends React.Component {
 };
 /***************************************************************************************************************************/
 // PASS A CALLBACK AS PROPS
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+  render() {
+    return (
+       <div>
+        { /* Change code below this line */ }
+          {/* Render GetInput and pass input and handleChange props */}
+        <GetInput
+          input={this.state.inputValue}
+          handleChange={this.handleChange}
+        />
+        {/* Render RenderInput and pass input prop */}
+        <RenderInput
+          input={this.state.inputValue}
+        />
+        { /* Change code above this line */ }
+       </div>
+    );
+  }
+};
+
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
+      </div>
+    );
+  }
+};
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
 
 /***************************************************************************************************************************/
 // USE LIFECYCLE METHOD COMPONENTWILLMOUNT
