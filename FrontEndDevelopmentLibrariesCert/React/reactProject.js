cefa34d72da0e3ce9b365c8a2194c7e15f4634c5
matthew123003 <matthew123003@gmail.com>
2024-08-24 13:31:14 -0400

    CompenentWillMount

--- FrontEndDevelopmentLibrariesCert/React/reactProject.js
+++ FrontEndDevelopmentLibrariesCert/React/reactProject.js
@@ -790,6 +790,19 @@ class RenderInput extends React.Component {
 
 /***************************************************************************************************************************/
 // USE LIFECYCLE METHOD COMPONENTWILLMOUNT
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+      console.log('MyComponent is about to mount');
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
 
 /***************************************************************************************************************************/
 // USE LIFECYCLE METHOD COMPENENTDIDMOUNT
