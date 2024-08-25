d9786551017a585c6956e0f71f16af7d9dd9c9a7
matthew123003 <matthew123003@gmail.com>
2024-08-25 11:38:17 -0400

    Use && operator

--- FrontEndDevelopmentLibrariesCert/React/reactProject.js
+++ FrontEndDevelopmentLibrariesCert/React/reactProject.js
@@ -1054,6 +1054,31 @@ class MyComponent extends React.Component {
 
 /***************************************************************************************************************************/
 // USE && FOR PRECISE CONDITIONAL
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState(state => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    // Change code below this line
+    return (
+      <div>
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+        {this.state.display && <h1>Displayed!</h1>}
+      </div>
+    );
+    // Change code above this line
+  }
+}
+
 
 /***************************************************************************************************************************/
 // TERNARY EXPRESSION FOR RENDERING
