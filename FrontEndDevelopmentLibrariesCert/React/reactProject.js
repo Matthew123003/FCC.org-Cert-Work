912b131ab7c878d6d420c575c1362ef4830656c7
matthew123003 <matthew123003@gmail.com>
2024-08-25 11:48:10 -0400

    Give sibling elements unique key attribute

--- FrontEndDevelopmentLibrariesCert/React/reactProject.js
+++ FrontEndDevelopmentLibrariesCert/React/reactProject.js
@@ -1267,6 +1267,29 @@ class MyToDoList extends React.Component {
 
 /***************************************************************************************************************************/
 // GIVE SIBLING ELEMENTS UNIQUE KEY ATTRIBUTE
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((framework) => (
+    <li key={framework}>{framework}</li>
+  ));
+ // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
 
 /***************************************************************************************************************************/
 // USE ARRAY.FILTER() TO DYNAMICALLY FILTER ARRAY
