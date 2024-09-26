7ecde577a5322aaa72e3324c734cfda398e11d38
matthew123003 <matthew123003@gmail.com>
2024-09-25 20:05:31 -0400

    Updated Jasmine Tests

--- JavaScriptJasmineTests/spec/support/SampleTests.spec.js
+++ JavaScriptJasmineTests/spec/support/SampleTests.spec.js
@@ -1,13 +1,27 @@
-describe("Array", function() {
-    it("should start empty", function() {
-      let arr = [];
-      expect(arr.length).toBe(0);
-    });
+describe("Sample Tests", function() {
+  // The `describe` function is used to group related tests. 
+  // It takes two arguments:
+  // 1. A string that describes the suite of tests (in this case, "Sample Tests").
+  // 2. A function that contains the tests (the "specs") to be executed within this group.
   
-    it("should add an item", function() {
-      let arr = [];
-      arr.push(1);
-      expect(arr.length).toBe(1);
-    });
+  it("should test a simple addition", function() {
+    // The `it` function is used to define an individual test case. 
+    // It also takes two arguments:
+    // 1. A string that describes what this specific test is supposed to do (in this case, "should test a simple addition").
+    // 2. A function that contains the actual test logic.
+    
+    expect(1 + 1).toBe(2);
+    // The `expect` function is used to create an assertion.
+    // Here, it checks if the expression `1 + 1` equals `2`.
+    // The `.toBe` matcher checks for strict equality (using ===).
   });
   
+  it("should test string concatenation", function() {
+    // Another test case defined using `it`.
+    // This one checks string concatenation.
+    
+    expect("Hello" + " World").toBe("Hello World");
+    // Similar to the previous assertion, this checks if 
+    // concatenating "Hello" and " World" equals "Hello World".
+  });
+});
