fbdd7dca2fed8d4629af5b7bf0cb1969701f0c47
matthew123003 <matthew123003@gmail.com>
2024-09-26 10:33:57 -0400

    Math Operations Jest

---
+++ JavaScriptJestTests/mathOperations.js
@@ -0,0 +1,11 @@
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  function concatenate(str1, str2) {
+    return str1 + str2;
+  }
+  
+  module.exports = { add, concatenate }; // Export functions for Jest
+  
